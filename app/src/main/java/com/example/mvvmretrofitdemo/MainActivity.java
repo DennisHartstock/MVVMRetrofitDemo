e099e1d24df4384a5a07ef64533a3b42310d2c13
DennsHartstock <dennis.hartstock@web.de>
2022-05-13 07:02:46 +0200

    get data from REST API

--- app/src/main/java/com/example/mvvmretrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmretrofitdemo/MainActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.mvvmretrofitdemo.model.MovieApiResponse;
+import com.example.mvvmretrofitdemo.service.MovieApiService;
+import com.example.mvvmretrofitdemo.service.RetrofitInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPopularMovies();
+    }
+
+    public void getPopularMovies() {
+        MovieApiService movieApiService = RetrofitInstance.getService();
+        Call<MovieApiResponse> call = movieApiService
+                .getPopularMovies(getString(R.string.api_key));
+        call.enqueue(new Callback<MovieApiResponse>() {
+            @Override
+            public void onResponse(Call<MovieApiResponse> call, Response<MovieApiResponse> response) {
+                MovieApiResponse movieApiResponse = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<MovieApiResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
